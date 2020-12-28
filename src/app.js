bdafe297143574617a3441b1dbd560e6131f8b74
Benjamin Dryden <benjamin@adventureclub.io>
2020-12-28 15:32:21 +0100

    final

--- src/app.js
+++ src/app.js
@@ -1,23 +1,16 @@
 class App {
   constructor() {
     this.rootElement = document.querySelector('body');
-    this.maskContainer = document.querySelector('.mask-container');
-
-
+    // this.maskContainer = document.querySelector('.mask-container');
     this.mousePos = {};
     window.addEventListener("mousemove", this.logMouseMove.bind(this));
-
   }
   logMouseMove(e) {
 	  this.mousePos.x = e.clientX;
 	  this.mousePos.y = e.clientY;
 	  this.rootElement.style.setProperty('--mouse-x', `${this.mousePos.x}px`);
 	  this.rootElement.style.setProperty('--mouse-y', `${this.mousePos.y}px`);
-
-    console.log(this.mousePos, 'mouse here');
+  }
 }
-
-}
-
 
 new App();
