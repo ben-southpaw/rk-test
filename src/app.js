e2007161b922684521f04db696dc878593d368f2
Benjamin Dryden <benjamin@adventureclub.io>
2020-12-28 14:01:31 +0100

    anims sort of working

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
-console.log('it works');
+class App {
+  constructor() {
+    this.rootElement = document.querySelector('body');
+    this.maskContainer = document.querySelector('.mask-container');
 
 
+    this.mousePos = {};
+    window.addEventListener("mousemove", this.logMouseMove.bind(this));
+
+  }
+  logMouseMove(e) {
+	  this.mousePos.x = e.clientX;
+	  this.mousePos.y = e.clientY;
+	  this.rootElement.style.setProperty('--mouse-x', `${this.mousePos.x}px`);
+	  this.rootElement.style.setProperty('--mouse-y', `${this.mousePos.y}px`);
+
+    console.log(this.mousePos, 'mouse here');
+}
+
+}
+
+
+new App();
